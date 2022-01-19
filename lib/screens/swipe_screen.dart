2e70ef330df663551b089bd3df02024e24afda74
ahmet <ahmetduser2000@gmail.com>
2022-01-19 22:40:08 +0100

    first commit, iteration 1

---
+++ lib/screens/swipe_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fynder_trial/shared/actions_menu.dart';
+import 'package:fynder_trial/shared/menu_bottom.dart';
+import 'package:fynder_trial/shared/menu_drawer.dart';
+
+class SwipeScreen extends StatelessWidget {
+  const SwipeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Image.asset('assets/FynderApplicationLogo.png', fit: BoxFit.cover),
+        actions: <Widget>[ActionsMenu()],
+      ),
+      bottomNavigationBar: MenuBottom(),
+      drawer: MenuDrawer(),
+      body: Center(
+        child: Text("Swipe Screen"),
+      ),
+    );
+  }
+}
---
+++ lib/shared/menu_bottom.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class MenuBottom extends StatelessWidget {
+  const MenuBottom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: (int index) {
+        switch (index) {
+          case 0:
+            Navigator.pushNamed(context, '/');
+            break;
+          case 1:
+            Navigator.pushNamed(context, '/Chat');
+            break;
+          case 2:
+            Navigator.pushNamed(context, '/NewsSection');
+            break;
+        }
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.swipe),
+          label: 'Swipe',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people_outline),
+          label: 'NewsSection',
+        ),
+      ],
+    );
+  }
+}
