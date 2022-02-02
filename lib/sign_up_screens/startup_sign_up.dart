c8e987bf368257e1f147f6f58b724751359b4139
Tudor <t.dumitru@lancaster.ac.uk>
2022-02-02 10:44:50 +0100

    changed startup signup page style

--- lib/sign_up_screens/startup_sign_up.dart
+++ lib/sign_up_screens/startup_sign_up.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:fynder/shared/actions_menu.dart';
 import 'package:fynder/shared/menu_bottom.dart';
 import 'package:fynder/shared/menu_drawer.dart';
+import 'package:fynder/textdesign/my_theme.dart';
 
 class StartupSingUp extends StatefulWidget {
   const StartupSingUp({Key? key}) : super(key: key);
@@ -29,86 +30,126 @@ class _StartupSingUpState extends State<StartupSingUp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:
-            Image.asset('assets/FynderApplicationLogo.png', fit: BoxFit.cover),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        inputDecorationTheme: MyTheme().theme(),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Text(
-                  'Welcome To Fynder',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Image.asset('assets/FynderApplicationLogo.png',
+              fit: BoxFit.cover),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Text(
+                    'Welcome To Fynder',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: TextField(
-                  controller: txtIdeaSummary,
-                  decoration: InputDecoration(hintText: 'Idea Summary'),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtIdeaSummary,
+                    decoration: InputDecoration(hintText: 'E-Mail'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: TextField(
-                  controller: txtPersonalWebsiteLink,
-                  decoration:
-                      InputDecoration(hintText: 'Personal Website Link'),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtPersonalWebsiteLink,
+                    decoration: InputDecoration(hintText: 'Password'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: TextField(
-                  controller: txtVideoLink,
-                  decoration: InputDecoration(hintText: 'Elevator Speech Link'),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtVideoLink,
+                    decoration: InputDecoration(hintText: 'Confirm Password'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: TextField(
-                  controller: txtTargetFunds,
-                  decoration: InputDecoration(hintText: 'Target Funds'),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtTargetFunds,
+                    decoration: InputDecoration(hintText: 'First Name'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: TextField(
-                  controller: txtMarketSegment,
-                  decoration: InputDecoration(hintText: 'Market Segment'),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtMarketSegment,
+                    decoration: InputDecoration(hintText: 'Last Name'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: TextField(
-                  controller: txtInvestmentType,
-                  decoration: InputDecoration(hintText: 'Investment Type'),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtInvestmentType,
+                    decoration: InputDecoration(hintText: 'Investment Type'),
+                  ),
                 ),
-              ),
-              MaterialButton(
-                minWidth: 370,
-                height: 60,
-                onPressed: () {
-
-                },
-                color: const Color(0xff0095FF),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50)),
-                child: const Text(
-                  'Save and Proceed',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtInvestmentType,
+                    decoration:
+                        InputDecoration(hintText: 'Personal Website Link'),
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtInvestmentType,
+                    decoration:
+                        InputDecoration(hintText: 'Elevator Speech Link'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtInvestmentType,
+                    decoration: InputDecoration(hintText: 'Target Funds'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtInvestmentType,
+                    decoration: InputDecoration(hintText: 'Market Segment'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    controller: txtInvestmentType,
+                    decoration: InputDecoration(hintText: 'Investment Type'),
+                  ),
+                ),
+                MaterialButton(
+                  minWidth: 370,
+                  height: 60,
+                  onPressed: () {},
+                  color: const Color(0xff0095FF),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  child: const Text(
+                    'Save and Proceed',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
