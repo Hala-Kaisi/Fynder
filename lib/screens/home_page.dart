5bad553681ed2fc34f17654fa3084d353dabaae5
ahmet <ahmetduser2000@gmail.com>
2022-02-26 16:57:42 +0100

    _initializeFirebase method moved to home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,99 +1,122 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fynder/screens/choose_account_type.dart';
 import 'package:fynder/screens/log_in.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+
+    User? user = FirebaseAuth.instance.currentUser;
+
+    // for removing to need to enter email and password again in the same phone
+    // if (user != null) {
+    //   Navigator.of(context).pushReplacement(
+    //     MaterialPageRoute(
+    //       builder: (context) => SwipeScreen(user: user),
+    //     ),
+    //   );
+    // }
+
+    return firebaseApp;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            // even space distribution
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  const Text(
-                    "Welcome",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 60,
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SafeArea(
+              child: Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                child: Column(
+                  // even space distribution
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        const Text(
+                          "Welcome",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 60,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "To Fynder ",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "To Fynder ",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 20,
+                    Container(
+                      height: MediaQuery.of(context).size.height / 3,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/welcome2.png"))),
                     ),
-                  )
-                ],
+                    Column(
+                      children: <Widget>[
+                        // the login button
+                        MaterialButton(
+                          color: Color(0Xff0095FF),
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (_) => LogIn()));
+                          },
+                          // defining the shape
+                          shape: RoundedRectangleBorder(
+                              // side: const BorderSide(color: Colors.black),
+                              borderRadius: BorderRadius.circular(50)),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18),
+                          ),
+                        ),
+                        // creating the signup button
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => ChooseAccount()));
+                          },
+                          child: const Text(
+                            "Sign up",
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/welcome2.png"))),
-              ),
-              Column(
-                children: <Widget>[
-                  // the login button
-                  MaterialButton(
-                    color: Color(0Xff0095FF),
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => LogIn()));
-                    },
-                    // defining the shape
-                    shape: RoundedRectangleBorder(
-                        // side: const BorderSide(color: Colors.black),
-                        borderRadius: BorderRadius.circular(50)),
-                    child: const Text(
-                      "Login",
-                      style:
-                          TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18),
-                    ),
-                  ),
-                  // creating the signup button
-                  const SizedBox(height: 20),
-                  MaterialButton(
-                    color: Color(0Xff0095FF),
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => ChooseAccount()));
-                    },
-                    // defining the shape
-                    shape: RoundedRectangleBorder(
-                      // side: const BorderSide(color: Colors.black),
-                        borderRadius: BorderRadius.circular(50)),
-                    child: const Text(
-                      "Sign Up",
-                      style:
-                      TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18),
-                    ),
-                  ),
-                  // creating the signup button
-                  const SizedBox(height: 20),
-
-                ],
-              )
-            ],
-          ),
-        ),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fynder/screens/swipe_screen.dart';
 import 'package:fynder/services/fire_auth.dart';
@@ -24,22 +23,6 @@ class _LogInState extends State<LogIn> {
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  Future<FirebaseApp> _initializeFirebase() async {
-    FirebaseApp firebaseApp = await Firebase.initializeApp();
-
-    User? user = FirebaseAuth.instance.currentUser;
-
-    // for removing to need to enter email and password again in the same phone
-    // if (user != null) {
-    //   Navigator.of(context).pushReplacement(
-    //     MaterialPageRoute(
-    //       builder: (context) => SwipeScreen(user: user),
-    //     ),
-    //   );
-    // }
-
-    return firebaseApp;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,127 +33,115 @@ class _LogInState extends State<LogIn> {
       },
       child: Scaffold(
         appBar: AppBar(title: const Text('Welcome')),
-        body: FutureBuilder(
-          future: _initializeFirebase(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 24.0),
+                child: const Text(
+                  "Welcome",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
+                  ),
+                ),
+              ),
+              Form(
+                key: _formKey,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 24.0),
-                      child: const Text(
-                        "Welcome",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 60,
+                    TextFormField(
+                      controller: emailController,
+                      focusNode: _focusEmail,
+                      validator: (value) => Validator.validateEmail(
+                        email: value!,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        errorBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                          ),
                         ),
                       ),
                     ),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            controller: emailController,
-                            focusNode: _focusEmail,
-                            validator: (value) => Validator.validateEmail(
-                              email: value!,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: "Email",
-                              errorBorder: UnderlineInputBorder(
-                                borderRadius: BorderRadius.circular(6.0),
-                                borderSide: BorderSide(
-                                  color: Colors.red,
-                                ),
-                              ),
-                            ),
+                    SizedBox(height: 12.0),
+                    TextFormField(
+                      controller: passwordController,
+                      focusNode: _focusPassword,
+                      obscureText: true,
+                      validator: (value) => Validator.validatePassword(
+                        password: value!,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        errorBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                          borderSide: BorderSide(
+                            color: Colors.red,
                           ),
-                          SizedBox(height: 12.0),
-                          TextFormField(
-                            controller: passwordController,
-                            focusNode: _focusPassword,
-                            obscureText: true,
-                            validator: (value) => Validator.validatePassword(
-                              password: value!,
-                            ),
-                            decoration: InputDecoration(
-                              hintText: "Password",
-                              errorBorder: UnderlineInputBorder(
-                                borderRadius: BorderRadius.circular(6.0),
-                                borderSide: BorderSide(
-                                  color: Colors.red,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 30.0),
-                          _isProcessing
-                              ? CircularProgressIndicator()
-                              : Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: MaterialButton(
-                                        onPressed: () async {
-                                          _focusEmail.unfocus();
-                                          _focusPassword.unfocus();
-
-                                          if (_formKey.currentState!
-                                              .validate()) {
-                                            setState(() {
-                                              _isProcessing = true;
-                                            });
-
-                                            User? user = await FireAuth
-                                                .signInUsingEmailPassword(
-                                              email: emailController.text,
-                                              password: passwordController.text,
-                                            );
-
-                                            setState(() {
-                                              _isProcessing = false;
-                                            });
-
-                                            if (user != null) {
-                                              Navigator.of(context)
-                                                  .pushReplacement(
-                                                MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      SwipeScreen(user: user),
-                                                ),
-                                              );
-                                            }
-                                          }
-                                        },
-                                        color: Color(0Xff0095FF),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(25),
-                                        ),
-                                        child: Text(
-                                          'Sign In',
-                                          style: TextStyle(color: Colors.white, fontSize: 18),
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(width: 30.0),
-                                  ],
-                                ),
-                        ],
+                        ),
                       ),
                     ),
+                    SizedBox(height: 30.0),
+                    _isProcessing
+                        ? CircularProgressIndicator()
+                        : Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: MaterialButton(
+                                  onPressed: () async {
+                                    _focusEmail.unfocus();
+                                    _focusPassword.unfocus();
+
+                                    if (_formKey.currentState!.validate()) {
+                                      setState(() {
+                                        _isProcessing = true;
+                                      });
+
+                                      User? user = await FireAuth
+                                          .signInUsingEmailPassword(
+                                        email: emailController.text,
+                                        password: passwordController.text,
+                                      );
+
+                                      setState(() {
+                                        _isProcessing = false;
+                                      });
+
+                                      if (user != null) {
+                                        Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                SwipeScreen(user: user),
+                                          ),
+                                        );
+                                      }
+                                    }
+                                  },
+                                  color: Color(0Xff0095FF),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Text(
+                                    'Sign In',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 30.0),
+                            ],
+                          ),
                   ],
                 ),
-              );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+              ),
+            ],
+          ),
         ),
       ),
     );
