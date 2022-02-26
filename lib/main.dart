30be3a40b195e605f99fddc46665f0c3152677ee
Hala <hala-kaissi@outlook.com>
2022-02-26 13:42:29 +0100

    updated main.dart to use firestore services

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fynder/screens/chat_screen.dart';
 import 'package:fynder/screens/home_page.dart';
@@ -5,7 +6,9 @@ import 'package:fynder/screens/news_section.dart';
 import 'package:fynder/screens/swipe_screen.dart';
 import 'package:fynder/textdesign/my_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Fynder());
 }
 
